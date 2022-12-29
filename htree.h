7984387cc0bb1a56ee398f71caa893d0b3f03e0c
Tuan Kiet <kiet.le0303@hcmut.edu.vn tuankiet0303@gmail.com>
2022-12-29 15:46:16 +0700

    complete constructor

--- htree.h
+++ htree.h
@@ -12,6 +12,7 @@ class HTree
 private:
     class Node;
     class Leaf;
+    struct Entry;
 
     int height{}; //theo dõi chiều cao của cây;
     Node* root{};
@@ -25,7 +26,40 @@ public:
                 {
                     return std::hash<FILETYPE>() (file) % NODE_SIZE; //some types.
                 }
+        
+        root = new Node(0, 1);
+        root->first_pointer = new Leaf(this->hash);
+        root->entries[0]    = new Leaf(this->hash);
     }
+
+    void insert(FILETYPE);
+    void search(FILETYPE);
+    void remove(FILETYPE);
+};
+
+template <typename FILETYPE>
+class HTree<FILETYPE>::Node
+{
+public:
+    void* first_pointer{};
+    Entry entries[NODE_SIZE - 1];
+    unsigned int numEntries{};
+    bool isLeaf = false;
+    std::uint8_t level{};
+
+    Node(std::uint8_t level, unsigned int numEntries = 1): first_pointer{nullptr}, numEntries{numEntries}, isLeaf{false}, level{level}
+    {}
+};
+
+template <typename FILETYPE>
+struct HTree<FILETYPE>::Entry
+{
+    void* child{};
+    int   key{};
+    bool isCollision = false;
+
+    Entry(): child{nullptr}, key{0}, isCollision{0}
+    {}
 };
 
 template <typename FILETYPE>
@@ -34,9 +68,10 @@ class HTree<FILETYPE>::Leaf
 public:
     FILETYPE value[NODE_SIZE];
     int size{};
-    int (*hash) (FILETYPE){};
+    int (*hash) (FILETYPE){}; //thừa hưởng của class
+    bool isLeaf = true;
 
-    Leaf(): size{0}
+    Leaf(int (*hash) (FILETYPE)): size{0}, hash{hash}
     {}
 
     void sort()
@@ -47,13 +82,10 @@ public:
                                        } 
                  );
     }
+
 };
 
-template <typename FILETYPE>
-class HTree<FILETYPE>::Node
-{
-public:
-    int size{};
-};
+
+
 
 #endif
